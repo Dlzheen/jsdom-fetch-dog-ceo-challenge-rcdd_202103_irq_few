d2c61799c12d3e5c033e974a743301ce14ce3b32
Dlzheen Mahmood <dlzheenmahmood@gmail.com>
2021-05-25 13:15:49 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HI', 'color: firebrick');
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = 'https://dog.ceo/api/breeds/list'
+
+window.addEventListener('DOMContentLoaded', () => {
+  fetch(imgUrl).then(result => {return result.json()})
+  .then(json =>{
+    for(let i=0;i<json.message.length;i++){
+    let image = document.createElement("img");
+    image.setAttribute("src",json.message[i]);
+    let imgDiv = document.querySelector("#dog-image-container"); 
+    imgDiv.append(image);
+    
+    }
+     
+  });
+   
+  fetch(breedUrl).then(result => {return result.json()})
+   .then(json =>{
+      for(let i=0;i<json.message.length;i++){
+        console.log(json.message[0]);     
+      }
+   
+     
+   }); 
+
+});
+
+   
